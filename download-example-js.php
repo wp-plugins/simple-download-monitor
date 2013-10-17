beb04f200d20933178063532cbef31eef4c03bc0
pepak.net <pepak.net@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-17 16:29:42 +0000

    čt 17.10.2013
    git-svn-id: https://plugins.svn.wordpress.org/simple-download-monitor/tags/0.24@789377 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ download-example-js.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+
+<div id="content">
+	<div id="content-inner">
+
+<div id="main">
+
+
+<div align="center">
+<br /><br />
+Your download will begin in 5 seconds! <br /> If it does not, <a href="/<?php echo $filename; ?>?download=true" rel="nofollow">click here</a>.
+<br /><br />
+</div>	
+
+		
+
+	
+</div> 
+<script>
+setTimeout("updateIframe()",5000);
+
+var exec = true;
+
+function updateIframe() { 
+	if(exec == true) { 
+		exec = false;
+		var ifrm = document.getElementById("frame1");
+		ifrm.src = "/<?php echo $filename; ?>?download=true";
+	}
+}
+</script>
+<?php get_sidebar(); ?>
+<iframe id="frame1" rel="nofollow" style="display:none"></iframe>
+<?php get_footer(); ?>
+
+
